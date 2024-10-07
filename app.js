ae9bc38a8b782f48468ede1981e301b083ca61b3
madhusudhananm <madhusudhananm1975@gmail.com>
2024-10-07 17:52:00 +0530

    'updatefunctionadded'

--- app.js
+++ app.js
@@ -10,10 +10,7 @@ const PORT = 3000;
 const url = 'mongodb+srv://testuser1:testUser123@animimic.nhuad.mongodb.net/?retryWrites=true&w=majority&appName=animimic'
 
 // Connect to MongoDB database
-mongoose.connect(url)
-.then(() => {
-  console.log('Connected to MongoDB');
-})
+mongoose.connect(url).then(() => { console.log('Connected to MongoDB'); })
 .catch((error) => {
   console.error('Error connecting to MongoDB:', error);
 });
@@ -24,7 +21,10 @@ const userSchema = new mongoose.Schema({
   mobile: String,
   username: String,
   email: String,
-  password: String
+  password: String,
+  secquestion: String,
+  secanswer: String,
+  photolink: String
 });
 
 // Create a User model based on the schema
@@ -52,6 +52,7 @@ const verifyToken = (req, res, next) => {
 // Route to register a new user
 app.post('/api/register', async (req, res) => {
   try {
+    var hashedSecAnswer;
     // Check if the email already exists
     if(req.body.email != ""){
       const existingUser = await User.findOne({ email: req.body.email });
@@ -69,6 +70,11 @@ app.post('/api/register', async (req, res) => {
 
     // Hash the password
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    if(req.body.secanswer != "")
+    {
+       hashedSecAnswer = await bcrypt.hash(req.body.secanswer, 10);
+
+    }
 
 
     // Create a new user
@@ -77,6 +83,9 @@ app.post('/api/register', async (req, res) => {
       email: req.body.email,
       name: req.body.name,
       mobile:req.body.mobile,
+      secquestion: req.body.secquestion,
+      secanswer: hashedSecAnswer,
+      photolink: req.body.photolink,
       password: hashedPassword
     });
     
@@ -100,16 +109,18 @@ app.post('/api/login', async (req, res) => {
     }
 
   } 
-  else {
+  else { //else use username instead of email. front end will ensure at least one of them is sent in api
        user = await User.findOne({ username: req.body.username });
       if (!user) {
         return res.status(401).json({ error: 'User Not Found' });
       }
     } 
+
     // Compare passwords
     const passwordMatch = await bcrypt.compare(req.body.password, user.password);
+
     if (!passwordMatch) {
-      return res.status(401).json({ error: 'Invalid credentials' });
+      return res.status(401).json({ error: 'Invalid credentials.' });
     }
 
     // Generate JWT token
@@ -122,6 +133,62 @@ app.post('/api/login', async (req, res) => {
   }
 });
 
+app.post('/api/update', verifyToken, async (req, res) => {// user can come to update screen only when logged in.
+  try{
+    if(req.body.name != "")
+    {
+      await User.updateOne( {email:req.body.email}, { $set: {name: req.body.name} } ) 
+    };
+    if(req.body.mobile != "")
+    {
+      await User.updateOne( {email:req.body.email}, { $set: {mobile: req.body.mobile} } ) 
+    };
+    if(req.body.secquestion != "")
+    {
+        await User.updateOne( {email:req.body.email}, { $set: {secquestion: req.body.secquestion} } ) 
+    };
+    if(req.body.secanswer != "")
+    {
+          const hashedSecAnswer = await bcrypt.hash(req.body.secanswer, 10);
+          await User.updateOne( {email:req.body.email}, { $set: {secanswer: hashedSecAnswer} } ) 
+    };
+    if(req.body.photolink != "")
+    {
+          await User.updateOne( {email:req.body.email}, { $set: {photolink: req.body.photolink} } ) 
+    };
+
+    
+    res.status(200).json({message: 'User updated'});
+  }
+  catch(error) {
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+
+app.post('/api/reset', verifyToken, async (req, res) => {// user can come to update screen only when logged in.
+  try{
+    const user = await User.findOne({ email: req.body.email });
+    if(req.body.secanswer != "")
+    {
+      const secanswermatch = await bcrypt.compare(req.body.secanswer, user.secanswer);
+      if (!secanswermatch) {
+        return res.status(401).json({ error: 'Invalid Answer to Security Question.' });
+      }
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+          await User.updateOne( {email:req.body.email}, { $set: {secanswer: hashedPassword} } ) 
+    };
+
+    
+    res.status(200).json({message: 'Pawssword updated'});
+  }
+  catch(error) {
+    res.status(500).json({ error: 'Internal server error' });
+  };
+});
+
+
+
 // Protected route to get user details
 app.get('/api/user', verifyToken, async (req, res) => {
   try {
@@ -130,7 +197,7 @@ app.get('/api/user', verifyToken, async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: 'User not found' });
     }
-    res.status(200).json({ username: user.username, email: user.email });
+    res.status(200).json({ username: user.username, email: user.email, mobile: user.mobile, name: user.name });
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
@@ -138,7 +205,7 @@ app.get('/api/user', verifyToken, async (req, res) => {
 
 // Default route
 app.get('/', (req, res) => {
-  res.send('Welcome to my User Registration and Login API!');
+  res.send('Welcome to Animimic API');
 });
 
 // Start the server
